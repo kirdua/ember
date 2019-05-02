03e984ce7ea6e6d405ca15ecf5db15b5b8c38ae5
Kirby Craft <kirby.craft@q2ebanking.com>
2019-05-02 14:54:28 -0500

    too fast

--- custom-setup/js/app.js
+++ custom-setup/js/app.js
@@ -4,9 +4,26 @@ var app = Ember.Application.create({
         LOG_TRANSITIONS:true
 });
 
+app.Router.map(function() {
+  this.route('about', { path:'/about-us' });
+});
+
 app.ApplicationController = Ember.Controller.extend({
   isCompany: true,
   hasWebsite: true,
-  firstName:"April",
-  lastName:"Brown"
+  firstName: "April",
+  lastName: "Brown",
+  caption: "Start Learning Today",
+  isDisabled: false,
+  color:"colortext",
+  actions: {
+    displayAlert: function(id) {
+      alert("Button Alert is clicked");
+      this.set('firstName', 'Olivia');
+    }
+  }
+});
+
+Ember.TextField.reopen({
+  attributeBindings: ['data-toggle', 'data-placement']
 });
