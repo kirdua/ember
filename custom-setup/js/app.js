e22289a4fa2f3b8638beacfec106e16b1196e220
Kirby Craft <kirby.craft@q2ebanking.com>
2019-05-03 08:28:25 -0500

    basic stuff

--- custom-setup/js/app.js
+++ custom-setup/js/app.js
@@ -24,6 +24,42 @@ app.ApplicationController = Ember.Controller.extend({
   }
 });
 
+app.ApplicationView = Ember.View.extend({
+  templateName: 'application',
+  name: 'test'
+});
+
+//creating model
+app.Course = DS.Model.extend({
+  title:DS.attr(),
+  price:DS.attr("number"),
+  description:DS.attr("string"),
+  image:DS.attr("string"),
+  summary: function(){
+    return this.get('title') + ' ' + this.get('description');
+  }.property('title', 'description')
+});
+
+app.IndexRoute = Ember.Route.extend({
+  model: function() {
+    this.store.push('course', {
+      id: "1",
+      title: "Master Ember.js",
+      price: 69,
+      description: "Learn Ember.js from scratch"
+    });
+
+    this.store.push('course', {
+      id: "2",
+      title: "Master React.js",
+      price: 69,
+      description: "Learn React.js from scratch"
+    });
+
+    return this.store.find('course', 2);
+  }
+});
+
 Ember.TextField.reopen({
   attributeBindings: ['data-toggle', 'data-placement']
 });
